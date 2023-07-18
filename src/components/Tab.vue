81caf0c7bfc3c299340554dbc5e8074b3356a1ba
SpirenkovArtur <arturspirenkov@gmail.com>
2023-07-18 21:03:03 +0300

    create Tab, Sidebar sfc. add mocks data

---
+++ src/components/Tab.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { Tab } from '../types'
+
+defineProps<{
+  tabs: Tab[]
+}>()
+
+</script>
+
+<template>
+  <div>
+    <nav class="flex space-x-7">
+      <a 
+        class="bg-movie-black-200 text-lg rounded-xl px-5 py-2 text-white drop-shadow-lg hover:bg-oranje"
+        v-for="{ title, emoji, id} in tabs"
+        :key="id"
+      >
+        {{ emoji }}
+        {{ title }}
+      </a>
+    </nav>
+  </div>
+</template>
---
+++ src/components/layout/Sidebar.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+import {
+  HomeIcon,
+  FilmIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/vue/24/outline"
+
+import {
+  HomeIcon as HomeSolidIcon,
+  FilmIcon as FilmSolidIcon,
+  MagnifyingGlassIcon as MagnifyingGlassSolidIcon,
+} from "@heroicons/vue/24/solid";
+
+const menu = [
+  {
+    to: '/',
+    icon: HomeIcon,
+    activeIcon: HomeSolidIcon,
+  },
+  {
+    to: '/search',
+    icon: MagnifyingGlassIcon,
+    activeIcon: MagnifyingGlassSolidIcon,
+  },
+  {
+    to: '/movies/1',
+    icon: FilmIcon,
+    activeIcon: FilmSolidIcon,
+  },
+]
+
+const route = useRoute()
+
+</script>
+
+<template>
+  <nav class="block bg-movie-black-200">
+    <div class="relative flex flex-col p-10 space-y-3">
+      <router-link 
+        v-for="{ to, icon, activeIcon } in menu" 
+        :to="to" 
+        :key="to"
+        :class="[
+          route.path === to
+            ? 'bg-movie-black-400 text-white'
+            : 'text-gray-600 hover:bg-movie-black-400',
+          'inline-flex items-center justify-center h-14 w-14 rounded-lg',
+        ]"
+      >
+        <component :is="route.path === to ? activeIcon : icon" class="h-10 w-10"></component>
+      </router-link>
+    </div>
+  </nav>
+</template>
