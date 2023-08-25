8462976acfe3f1ece954b8350627e954bc8b64ac
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-25 16:21:29 +0300

    change Tab sfc :TODO

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -1,19 +1,29 @@
 <script lang="ts" setup>
-import { Tab } from '../types'
-
+import { ITab } from '../types'
+//TODO: destructurization on Tab and Tabs
 defineProps<{
-  tabs: Tab[]
+  tabs: ITab[],
+  activeTab: string
 }>()
 
+interface Emits {
+  (e: 'tab-change', tabs: string): void
+}
+
+const emit = defineEmits<Emits>()
 </script>
 
 <template>
   <div>
     <nav class="flex space-x-7">
-      <a 
-        class="bg-movie-black-200 text-lg rounded-xl px-5 py-2 text-white drop-shadow-lg hover:bg-oranje"
-        v-for="{ title, emoji, id} in tabs"
-        :key="id"
+      <a class="
+        text-lg rounded-2xl px-7 py-3 text-white drop-shadow-lg hover:bg-orange cursor-pointer  
+        transition delay-150 select-none
+        "        
+        :class="id === activeTab ? 'bg-orange' : 'bg-movie-black-200'"
+        v-for="{ title, emoji, id } in tabs" 
+        :key="id" 
+        @click="emit('tab-change', id)"
       >
         {{ emoji }}
         {{ title }}
