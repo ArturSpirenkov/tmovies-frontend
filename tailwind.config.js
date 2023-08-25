9f1b60cefdaf3d46d1e206600f62251b53847824
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-25 16:01:46 +0300

    add lib vue-carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         "movie-black": "#1d2024",
         "movie-black-200": "#1f2326",
         "movie-black-400": "#282c31",
-        "oranje": "#f37515"
+        "orange": "#f37515"
       }
     },
   },
