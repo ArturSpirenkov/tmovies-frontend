9eac560611c77b2f217da07a6cade4784ee6e026
ArturSpirenkov <arturspirenkov@gmail.com>
2023-08-25 18:55:02 +0300

    add mocks data

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,12 @@ export interface IFlashNews  {
   text: string;
   date: string;
 }
+
+export interface IMovieFeed {
+  title: string
+  poster: string
+  year: string
+  id: string
+  rate: string
+  link?: string
+}
